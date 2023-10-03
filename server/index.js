8c229ff553e2f423084bc48c1150efd64c95ba6f
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2023-10-04 03:30:48 +0530

    Socket.io server added

--- server/index.js
+++ server/index.js
@@ -1,27 +1,76 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const adminRouter = require("./routes/admin");
-const userRouter = require("./routes/user");
-const path = require("path");
-const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
 
-app.use(cors());
-app.use(express.json());
+const server = http.createServer();
 
-app.use("/admin", adminRouter);
-app.use("/user", userRouter);
+//emit -> sender
+//on -> receiver
 
-app.use(express.static("public"));
-app.use("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+let questions = [];
+io.on("connection", (socket) => {
+  setTimeout(() => {
+    socket.emit(
+      "questions",
+      questions.filter((que) => !que.answered)
+    );
+  }, 200);
+
+  socket.on("ques", (data) => {
+    questions.push({
+      id: questions.length,
+      username: data.username,
+      ques: data.ques,
+      upvotes: 1,
+      upvoters: [socket.id],
+      answered: false,
+    });
+    questions.sort((a, b) => b.upvoters.length - a.upvoters.length);
+    console.log(questions);
+    io.emit(
+      "questions",
+      questions.filter((que) => !que.answered)
+    );
+  });
+  socket.on("upvote", (data) => {
+    questions.forEach((que) => {
+      if (que.id === data.id) {
+        if (!que.upvoters.includes(socket.id)) {
+          que.upvoters.push(socket.id);
+        }
+      }
+    });
+    questions.sort((a, b) => b.upvoters.length - a.upvoters.length);
+    console.log(questions);
+    io.emit(
+      "questions",
+      questions.filter((que) => !que.answered)
+    );
+  });
+  socket.on("deleteall", () => {
+    questions = [];
+    io.emit("questions", questions);
+  });
+  socket.on("answered", (data) => {
+    questions.forEach((que) => {
+      if (que.id === data.id) {
+        que.answered = true;
+      }
+    });
+    questions.sort((a, b) => b.upvoters.length - a.upvoters.length);
+    console.log(questions);
+    io.emit(
+      "questions",
+      questions.filter((que) => !que.answered)
+    );
+  });
 });
 
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-mongoose.connect(
-  "mongodb+srv://kirattechnologies:iRbi4XRDdM7JMMkl@cluster0.e95bnsi.mongodb.net/admin?authSource=admin&replicaSet=atlas-ue73sj-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
-  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" }
-);
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(8000, () => {
+  console.log("Server is running on port : 8000");
+});
